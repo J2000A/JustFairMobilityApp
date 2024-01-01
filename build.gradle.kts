109fbeda1ee856fed9b3a577f3625a0248a68125
J2000A <84895687+J2000A@users.noreply.github.com>
2024-01-01 18:53:42 +0100

    Configure Maps API

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,10 @@
 plugins {
     id("com.android.application") version "8.1.3" apply false
 }
+
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    }
+}
