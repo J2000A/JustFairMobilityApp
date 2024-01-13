f124c23ce65a7b7da85a25c04895a33563b2ccff
J2000A <84895687+J2000A@users.noreply.github.com>
2024-01-13 01:35:36 +0100

    Add Privacy and Terms

--- app/src/main/java/de/jonasaugust/justfairmobilityapp/activities/SettingsActivity.java
+++ app/src/main/java/de/jonasaugust/justfairmobilityapp/activities/SettingsActivity.java
@@ -1,5 +1,7 @@
 package de.jonasaugust.justfairmobilityapp.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
@@ -8,18 +10,23 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 
 import de.jonasaugust.justfairmobilityapp.R;
+import de.jonasaugust.justfairmobilityapp.helpers.view_builders.toasts.ToastBuilder;
 
 // TODO Implement
 public class SettingsActivity extends ActivityRoot {
 
     private TextView title;
     private View back;
+    private View privacy;
+    private View terms;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         title = findViewById(R.id.title);
         back = findViewById(R.id.back);
+        privacy = findViewById(R.id.privacy);
+        terms = findViewById(R.id.terms);
     }
 
     @Override
@@ -30,6 +37,16 @@ public class SettingsActivity extends ActivityRoot {
     @Override
     protected void setListeners() {
         back.setOnClickListener(view -> onBackPressed());
+        privacy.setOnClickListener(view -> openUrl(getString(R.string.privacy_url)));
+        terms.setOnClickListener(view -> openUrl(getString(R.string.terms_url)));
+    }
+
+    private void openUrl(String url) {
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+        } catch (Exception e) {
+            ToastBuilder.show(this, e);
+        }
     }
 
     @Override
@@ -49,6 +66,6 @@ public class SettingsActivity extends ActivityRoot {
 
     @Override
     protected int getContentView() {
-        return R.layout.activity_todo;
+        return R.layout.activity_settings;
     }
 }
